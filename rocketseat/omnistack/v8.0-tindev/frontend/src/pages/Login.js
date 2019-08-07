c2b28fd34b0bd5b3e407f51a96b9fdaa300caca9
Fabrício <fabriciosantosb@gmail.com>
2019-08-07 18:17:47 -0300

    Getting the form value: initial test with console.log

--- rocketseat/omnistack/v8.0-tindev/frontend/src/pages/Login.js
+++ rocketseat/omnistack/v8.0-tindev/frontend/src/pages/Login.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 
 import logo from "../assets/logo.svg";
 import { isContainer } from "postcss-selector-parser";
 
 export default function Login() {
+  const [username, setUsername] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    console.log(username);
+  }
+
   return (
     <div className="login-container">
-      <form>
+      <form onSubmit={handleSubmit}>
         <img src={logo} alt="Tindev logo" />
-        <input placeholder="type your Github username" />
+        <input
+          placeholder="type your Github username"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+        />
         <button type="submit">Log in</button>
       </form>
     </div>
