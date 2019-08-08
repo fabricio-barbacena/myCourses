4d3d304880ae248357af1c4d666432ff7dc68b36
Fabrício <fabriciosantosb@gmail.com>
2019-08-07 21:09:52 -0300

    Cards structure in Main designed

--- rocketseat/omnistack/v8.0-tindev/frontend/src/pages/Main.js
+++ rocketseat/omnistack/v8.0-tindev/frontend/src/pages/Main.js
@@ -1,5 +1,94 @@
 import React from "react";
 
+import logo from "../assets/logo.svg";
+import like from "../assets/like.svg";
+import dislike from "../assets/dislike.svg";
+
 export default function Main({ match }) {
-  return <h1>{match.params.id}</h1>;
+  return (
+    <div className="main-container">
+      <img src={logo} alt="Tindev logo" />
+      <ul>
+        <li>
+          <img
+            src="https://avatars0.githubusercontent.com/u/4248081?v=4"
+            alt="avatar"
+          />
+          <footer>
+            <strong>Filipe Deschamps</strong>
+            <p>Programador e cantor sertanejo nas horas vagas</p>
+          </footer>
+
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="dislike" />
+            </button>
+            <button type="button">
+              <img src={like} alt="like" />
+            </button>
+          </div>
+        </li>
+
+        <li>
+          <img
+            src="https://avatars0.githubusercontent.com/u/4248081?v=4"
+            alt="avatar"
+          />
+          <footer>
+            <strong>Filipe Deschamps</strong>
+            <p>Programador e cantor sertanejo nas horas vagas</p>
+          </footer>
+
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="dislike" />
+            </button>
+            <button type="button">
+              <img src={like} alt="like" />
+            </button>
+          </div>
+        </li>
+
+        <li>
+          <img
+            src="https://avatars0.githubusercontent.com/u/4248081?v=4"
+            alt="avatar"
+          />
+          <footer>
+            <strong>Filipe Deschamps</strong>
+            <p>Programador e cantor sertanejo nas horas vagas</p>
+          </footer>
+
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="dislike" />
+            </button>
+            <button type="button">
+              <img src={like} alt="like" />
+            </button>
+          </div>
+        </li>
+
+        <li>
+          <img
+            src="https://avatars0.githubusercontent.com/u/4248081?v=4"
+            alt="avatar"
+          />
+          <footer>
+            <strong>Filipe Deschamps</strong>
+            <p>Programador e cantor sertanejo nas horas vagas</p>
+          </footer>
+
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="dislike" />
+            </button>
+            <button type="button">
+              <img src={like} alt="like" />
+            </button>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
 }
