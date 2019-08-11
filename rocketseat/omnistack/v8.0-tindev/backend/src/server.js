bcb953061bc9a690b13a4f81c6c782624364b19e
Fabrício <fabriciosantosb@gmail.com>
2019-08-11 09:11:25 -0300

    Changed: getting user id and socket id in console.log

--- rocketseat/omnistack/v8.0-tindev/backend/src/server.js
+++ rocketseat/omnistack/v8.0-tindev/backend/src/server.js
@@ -8,18 +8,14 @@ const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 
+const connectedUsers = {};
+
 io.on("connection", socket => {
-  console.log("New connection", socket.id);
+  const { user } = socket.handshake.query;
 
-  socket.on("hello", message => {
-    console.log(message);
-  });
+  console.log(user, socket.id);
 
-  setTimeout(() => {
-    socket.emit("world", {
-      message: "Omnistack"
-    });
-  }, 5000);
+  connectedUsers[user] = socket.id;
 });
 
 mongoose.connect(
