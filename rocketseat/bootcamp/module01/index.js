27ff8887dc2bd9c408a7fe1fd52b72bffc88be10
Fabrício <fabriciosantosb@gmail.com>
2019-08-12 13:54:49 -0300

    Added: examples of query params and route params

--- rocketseat/bootcamp/module01/index.js
+++ rocketseat/bootcamp/module01/index.js
@@ -4,8 +4,28 @@ const server = express();
 
 server.use(express.json());
 
-server.get("/", (req, res) => {
+// get route returning message
+server.get("/text", (req, res) => {
+  return res.send("Hello, World!");
+});
+
+// get route returning json
+server.get("/json", (req, res) => {
   return res.json({ message: "Hello, World!" });
+});
+
+// get route with query params
+server.get("/name", (req, res) => {
+  const { name } = req.query;
+
+  return res.json(`message: Hello, ${name}`);
+});
+
+// get rout with route params
+server.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+
+  return res.json({ message: `Searching for user ${id}` });
 });
 
 server.listen("3333");
