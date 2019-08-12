c294bcd3abefdc711d63eacb26c44fd8820ae19f
Fabrício <fabriciosantosb@gmail.com>
2019-08-12 14:21:15 -0300

    Route params with index of a list; nodemon installed

--- rocketseat/bootcamp/module01/index.js
+++ rocketseat/bootcamp/module01/index.js
@@ -21,11 +21,19 @@ server.get("/name", (req, res) => {
   return res.json(`message: Hello, ${name}`);
 });
 
-// get rout with route params
-server.get("/users/:id", (req, res) => {
+// get route with route params
+server.get("/user/:id", (req, res) => {
   const { id } = req.params;
 
   return res.json({ message: `Searching for user ${id}` });
+});
+
+const users = ["Diego", "Claudio", "Victor"];
+
+server.get("/users/:index", (req, res) => {
+  const { index } = req.params;
+
+  return res.json({ user: `${users[index]}` });
 });
 
 server.listen("3333");
