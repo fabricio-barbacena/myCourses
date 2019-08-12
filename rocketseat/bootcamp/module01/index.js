2fb850ac90b2a23056e3c65b4996c6cb1b0e29e0
Fabrício <fabriciosantosb@gmail.com>
2019-08-12 12:24:09 -0300

    Changed: returning a json in a get route

--- rocketseat/bootcamp/module01/index.js
+++ rocketseat/bootcamp/module01/index.js
@@ -5,7 +5,7 @@ const server = express();
 server.use(express.json());
 
 server.get("/", (req, res) => {
-  return res.send("Hello, World!");
+  return res.json({ message: "Hello, World!" });
 });
 
 server.listen("3333");
