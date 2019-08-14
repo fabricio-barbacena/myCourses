056fbdcbede6c3e6d52447868b4c7b2ec4ccf3ac
Fabrício <fabriciosantosb@gmail.com>
2019-08-14 15:56:29 -0300

    Added: eslint, prettier, .editorconfig, sequelize; .sequelizerc finished

--- rocketseat/bootcamp/module02/src/app.js
+++ rocketseat/bootcamp/module02/src/app.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import routes from './routes';
 
-class App() {
+class App {
   constructor() {
     this.server = express();
-    
+
     this.middlewares();
     this.routes();
   }
