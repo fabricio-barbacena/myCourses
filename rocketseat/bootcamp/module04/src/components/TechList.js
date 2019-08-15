0e729de79a4e55dec52e533020e3091df4fd99aa
Fabrício <fabriciosantosb@gmail.com>
2019-08-15 18:36:42 -0300

    Added: input to new tech and console.log

--- rocketseat/bootcamp/module04/src/components/TechList.js
+++ rocketseat/bootcamp/module04/src/components/TechList.js
@@ -2,16 +2,24 @@ import React, { Component } from "react";
 
 class TechList extends Component {
   state = {
+    newTech: "",
     techs: ["Node.js", "ReactJS", "React Native"]
+  };
+
+  handleInputChange = e => {
+    console.log(e.target.value);
   };
 
   render() {
     return (
-      <ul>
-        {this.state.techs.map(tech => (
-          <li key={tech}>{tech}</li>
-        ))}
-      </ul>
+      <>
+        <ul>
+          {this.state.techs.map(tech => (
+            <li key={tech}>{tech}</li>
+          ))}
+        </ul>
+        <input type="text" onChange={this.handleInputChange} />
+      </>
     );
   }
 }
