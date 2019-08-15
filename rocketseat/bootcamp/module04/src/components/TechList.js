1ed716a07c3ebcaa61956cf6f73782648b8d7ddc
Fabrício <fabriciosantosb@gmail.com>
2019-08-15 18:52:57 -0300

    Added: new tech logged in the browser after pressing send button

--- rocketseat/bootcamp/module04/src/components/TechList.js
+++ rocketseat/bootcamp/module04/src/components/TechList.js
@@ -10,17 +10,28 @@ class TechList extends Component {
     this.setState({ newTech: e.target.value });
   };
 
+  handleSubmit = e => {
+    e.preventDefault();
+
+    console.log(this.state.newTech);
+  };
+
   render() {
     return (
-      <>
+      <form onSubmit={this.handleSubmit}>
         <h1>{this.state.newTech}</h1>
         <ul>
           {this.state.techs.map(tech => (
             <li key={tech}>{tech}</li>
           ))}
         </ul>
-        <input type="text" onChange={this.handleInputChange} />
-      </>
+        <input
+          type="text"
+          onChange={this.handleInputChange}
+          value={this.state.newTech}
+        />
+        <button type="submit">Send</button>
+      </form>
     );
   }
 }
