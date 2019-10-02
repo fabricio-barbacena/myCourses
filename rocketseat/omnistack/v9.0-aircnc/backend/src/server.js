b7f56f9a84ef6abfa6d4dad0f19cb563909e6e73
Fabrício <fabriciosantosb@gmail.com>
2019-10-02 14:35:25 -0300

    Added: mongoose and mongodb atlas database connection

--- rocketseat/omnistack/v9.0-aircnc/backend/src/server.js
+++ rocketseat/omnistack/v9.0-aircnc/backend/src/server.js
@@ -1,7 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const routes = require("./routes");
 
 const app = express();
+
+mongoose.connect(
+  "mongodb+srv://fabricio2:omnistack@cluster0-cgpse.mongodb.net/Omnistack9?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
 
 app.use(express.json());
 app.use(routes);
