b54001f48d5cb359362f9bd2c5a2ff63dbc42521
Fabrício <fabriciosantosb@gmail.com>
2019-10-02 13:49:19 -0300

    Added: example of get route with query params

--- rocketseat/omnistack/v9.0-aircnc/backend/src/server.js
+++ rocketseat/omnistack/v9.0-aircnc/backend/src/server.js
@@ -6,4 +6,8 @@ app.get("/hello", (req, res) => {
   return res.json({ message: "Hello, World!" });
 });
 
+app.get("/users", (req, res) => {
+  return res.json({ user: req.query.user });
+});
+
 app.listen(3333);
